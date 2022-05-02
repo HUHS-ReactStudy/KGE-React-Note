29a453c742abeb43a5ca861d9578289d301c872a
yellowOctopus <greencloud11@hanyang.ac.kr>
2022-05-02 12:14:08 +0900

    chapter10

---
+++ 9장_컴포넌트 스타일링/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SassComponent from './Sass';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 9장_컴포넌트 스타일링/Sass.js
@@ -0,0 +1,14 @@
+import './Sass.scss';
+
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box red" />
+      <div className="box orange" />
+      <div className="box yellow" />
+      <div className="box green" />
+    </div>
+  );
+};
+
+export default SassComponent;
